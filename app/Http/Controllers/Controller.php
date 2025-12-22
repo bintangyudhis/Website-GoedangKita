14de3d09dbeb52503bb861d04c07f639a19d7e3c
bintangyudhis <bintangyudhis333@gmail.com>
2025-12-22 20:53:38 +0700

    menambahkan komentar pada controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,13 +1,13 @@
-<?php
+<?php // Tag pembuka PHP untuk file controller dasar (base controller)
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers; // Namespace utama untuk semua controller aplikasi
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests; // Trait untuk fitur authorization (policy/gate) pada controller
+use Illuminate\Foundation\Bus\DispatchesJobs; // Trait untuk menjalankan/dispatch job (queue) dari controller
+use Illuminate\Foundation\Validation\ValidatesRequests; // Trait untuk memvalidasi request (validate()) di controller
+use Illuminate\Routing\Controller as BaseController; // Mengimpor base controller Laravel dan memberi alias BaseController
 
-class Controller extends BaseController
+class Controller extends BaseController // Kelas controller dasar yang akan diwarisi oleh semua controller lain
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-}
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests; // Mengaktifkan fitur authorize, dispatch job, dan validasi request
+} // Penutup class Controller
